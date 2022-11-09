366e4eeccd8fc1a575e7ee8c77cac65f1eb33f1f
nopnop2002 <hau26280@tree.odn.ne.jp>
2022-11-09 16:36:16 +0900

    Changed log format for int32/uint32_t

--- esp-idf-irSend-StickC/main/st7735s.c
+++ esp-idf-irSend-StickC/main/st7735s.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <inttypes.h>
 #include <string.h>
 #include <math.h>
 
@@ -146,7 +148,7 @@ bool spi_master_write_color(ST7735_t * dev, uint16_t color, uint16_t size)
 void delayMS(int ms) {
 	int _ms = ms + (portTICK_PERIOD_MS - 1);
 	TickType_t xTicksToDelay = _ms / portTICK_PERIOD_MS;
-	ESP_LOGD(TAG, "ms=%d _ms=%d portTICK_PERIOD_MS=%d xTicksToDelay=%d",ms,_ms,portTICK_PERIOD_MS,xTicksToDelay);
+	ESP_LOGD(TAG, "ms=%d _ms=%d portTICK_PERIOD_MS=%"PRIu32" xTicksToDelay=%"PRIu32,ms,_ms,portTICK_PERIOD_MS,xTicksToDelay);
 	vTaskDelay(xTicksToDelay);
 }
 
